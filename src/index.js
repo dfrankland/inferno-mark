f87da3036636fee3dd37ffc9b34e1a9195351fce
Dylan Frankland <dylan@frankland.io>
2017-02-08 23:42:54 -0800

    Add ability to create HoC

--- src/index.js
+++ src/index.js
@@ -19,57 +19,67 @@ const hyperscriptWrapper = components => (name = 'div', props = {}, children = [
   return infernoHyperscript(nameLowerCase, props, children);
 };
 
-const createMark = getHastFromString => (
-  (strings, ...valueArgs) => (
-    (props = {}) => {
-      // Construct tagged template string with props
-      const values = [...valueArgs];
-      let string = '';
-      for (let i = 0; i < strings.length; i += 1) {
-        string += strings[i];
-        if (values[i]) string += values[i](props);
-      }
-
-      // Set defaults and deconstruct options
-      let infernoMarkOptions = props.infernoMarkOptions;
-      if (
-        Array.isArray(infernoMarkOptions) ||
-        typeof props.infernoMarkOptions !== 'object'
-      ) infernoMarkOptions = {};
-      const {
-        plugins: userPlugins,
-        wrapper,
-        sanitize = true,
-        sanitizeSchema,
-        components: userComponents,
-        prefix,
-      } = infernoMarkOptions;
-
-      // Pass user plugins and constuct hast
-      const plugins = Array.isArray(userPlugins) ? userPlugins : [];
-      const hast = getHastFromString(string, plugins);
-
-      // Wrap hast and optionally sanitize it with a schema
-      let newHast = div(hast.children);
-      if (sanitize) {
-        let schema = sanitizeSchema;
-        if (Array.isArray(schema) || typeof sanitizeSchema !== 'object') schema = null;
-        newHast = hastSanitize(newHast, schema);
-        if (newHast.type === 'root') newHast = div(newHast.children);
-      }
-
-      const components = (
-        Array.isArray(userComponents) ||
-        typeof userComponents !== 'object'
-      ) ? {} : userComponents;
-      let newHyperscript = hastToHyperscript(hyperscriptWrapper(components), newHast, prefix);
-      if (typeof wrapper === 'function') {
-        newHyperscript = wrapper({ ...props, children: newHyperscript.children });
-      }
-
-      return newHyperscript;
+const taggedTemplate = (getHastFromString, wrapper) => (strings, ...valueArgs) => (
+  (props = {}) => {
+    // Construct tagged template string with props
+    const values = [...valueArgs];
+    let string = '';
+    for (let i = 0; i < strings.length; i += 1) {
+      string += strings[i];
+      if (values[i]) string += values[i](props);
     }
-  )
+
+    // Set defaults and deconstruct options
+    let infernoMarkOptions = props.infernoMarkOptions;
+    if (
+      Array.isArray(infernoMarkOptions) ||
+      typeof props.infernoMarkOptions !== 'object'
+    ) infernoMarkOptions = {};
+    const {
+      plugins: userPlugins,
+      wrapper: wrapperOverride = wrapper,
+      sanitize = true,
+      sanitizeSchema,
+      components: userComponents,
+      prefix,
+    } = infernoMarkOptions;
+
+    // Pass user plugins and constuct hast
+    const plugins = Array.isArray(userPlugins) ? userPlugins : [];
+    const hast = getHastFromString(string, plugins);
+
+    // Wrap hast and optionally sanitize it with a schema
+    let newHast = div(hast.children);
+    if (sanitize) {
+      let schema = sanitizeSchema;
+      if (Array.isArray(schema) || typeof sanitizeSchema !== 'object') schema = null;
+      newHast = hastSanitize(newHast, schema);
+      if (newHast.type === 'root') newHast = div(newHast.children);
+    }
+
+    const components = (
+      Array.isArray(userComponents) ||
+      typeof userComponents !== 'object'
+    ) ? {} : userComponents;
+    let newHyperscript = hastToHyperscript(hyperscriptWrapper(components), newHast, prefix);
+    if (typeof wrapperOverride === 'function') {
+      newHyperscript = wrapperOverride({ ...props, children: newHyperscript.children });
+    }
+
+    return newHyperscript;
+  }
+);
+
+const createMark = getHastFromString => (
+  (stringsOrComponent, ...valueArgs) => {
+    // HoC
+    if (typeof stringsOrComponent === 'function') {
+      return taggedTemplate(getHastFromString, stringsOrComponent);
+    }
+
+    // Tagged Template
+    return taggedTemplate(getHastFromString)(stringsOrComponent, ...valueArgs);
+  }
 );
 
 const reducePlugins = (processor, plugins) => {
